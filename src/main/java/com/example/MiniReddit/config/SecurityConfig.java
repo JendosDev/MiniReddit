b45e3a970b4d54a410345777481679a709be3eac
JendosDev <vesjan007@gmail.com>
2023-07-09 09:21:59 +0200

    Branch

--- src/main/java/com/example/MiniReddit/config/SecurityConfig.java
+++ src/main/java/com/example/MiniReddit/config/SecurityConfig.java
@@ -1,21 +1,42 @@
 package com.example.MiniReddit.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
-                .authorizeRequests()
-                .requestMatchers("/api/auth/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated();
-        return httpSecurity.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.cors().and()
+                .csrf().disable()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/auth/**")
+                        .permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/subreddit")
+                        .permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/posts/")
+                        .permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/posts/**")
+                        .permitAll()
+                        .requestMatchers("/v2/api-docs",
+                                "/configuration/ui",
+                                "/swagger-resources/**",
+                                "/configuration/security",
+                                "/swagger-ui.html",
+                                "/webjars/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()).build();
     }
 }
--- src/main/java/com/example/MiniReddit/model/Subreddit.java
+++ src/main/java/com/example/MiniReddit/model/Subreddit.java
@@ -1,7 +1,6 @@
 package com.example.MiniReddit.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +9,9 @@ import lombok.NoArgsConstructor;
 import java.time.Instant;
 import java.util.List;
 
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,17 +19,14 @@ import java.util.List;
 @Builder
 
 public class Subreddit {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @NotBlank(message = "Community name is required")
-    private String name;
-    @NotBlank(message = "Description is required")
-    private String description;
-    @ManyToOne(fetch = FetchType.LAZY)
-    private List<Post> posts;
-    private Instant createdDate;
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User user;
-
-}
+        @Id
+        @GeneratedValue(strategy = IDENTITY)
+        private Long id;
+        private String name;
+        private String description;
+        @OneToMany(fetch = LAZY)
+        private List<Post> posts;
+        private Instant createdDate;
+        @ManyToOne(fetch = LAZY)
+        private User user;
+    }
